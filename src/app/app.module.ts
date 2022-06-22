d85815b7b71b7ea960ed57f42cda7d85ee861f5c
Nicholas Torres <nicholastorres@gmx.com>
2022-06-22 13:27:26 -0400

    imported components to app.module.ts, for testing of components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './Modules/material/material.module';
+import { TimerComponent } from './timer/timer/timer.component';
+import { CreateTimerComponent } from './timer/create-timer/create-timer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimerComponent,
+    CreateTimerComponent
   ],
   imports: [
     BrowserModule,
