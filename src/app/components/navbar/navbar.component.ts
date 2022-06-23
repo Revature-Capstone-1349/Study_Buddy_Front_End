f4c662fdfdceb798af73e1f8392e6c44acccac99
Daniel Vais <daniel928@revature.net>
2022-06-23 08:54:29 -0700

    navbar update

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
+  isLogged : boolean = false;
+  constructor(private cookie: CookieService) { }
 
   ngOnInit(): void {
+    this.isLogged = this.cookie.check("userId")
   }
+
+  // onCartClk(){
+  //   //console.log("works")
+  //   if(this.cookie.check("CartDisplay")){
+  //     if(this.cookie.get("CartDisplay") == "true"){
+  //       this.cookie.set("CartDisplay", "false");
+  //     }
+  //     else{
+  //       this.cookie.set("CartDisplay", "true");
+  //       }
+  //   }
+  //   else{
+  //     this.cookie.set("CartDisplay", "false")
+  //   }
+  //    console.log(this.cookie.get("CartDisplay"))
+  // }
 
 }
