77c30c25a4188e16f855160e5fd71fd5257fc4cf
Nicholas Torres <nicholastorres@gmx.com>
2022-06-24 10:15:17 -0400

    pushing router...

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+<<<<<<< Updated upstream
 import { RouterModule, Routes } from '@angular/router';
 import { TimerComponent } from './timer/timer/timer.component';
 import { CreateTimerComponent } from './timer/create-timer/create-timer.component';
@@ -9,6 +10,25 @@ const routes: Routes = [
   { path: 'creattimer', component: CreateTimerComponent },
   { path: 'timer', component: TimerComponent}
 ]
+=======
+import { Router, RouterModule, Routes } from '@angular/router';
+import { TimerComponent } from './Components/timer/timer.component';
+import { LoginComponent } from "./Components/login/login.component";
+import { RegisterComponent } from "./Components/register/register.component";
+import { ErrorPageComponent } from "./Pages/error-page/error-page.component";
+import { HomePageComponent } from "./Pages/home-page/home-page.component";
+
+
+
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'timer', component: TimerComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomePageComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '**', component: ErrorPageComponent }
+];
+>>>>>>> Stashed changes
 
   @NgModule({
     imports: [RouterModule.forRoot(routes)],
--- src/app/timer/timer/timer.component.ts
+++ src/app/timer/timer/timer.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { MatSnackBar } from '@angular/material/snack-bar';
+import { Component, Inject, OnInit, NgModule } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { TimerService } from 'src/app/timer.service';
+import { CreateTimerComponent } from '../create-timer/create-timer.component';
 
 @Component({
   selector: 'app-timer',
@@ -9,8 +10,19 @@ import { TimerService } from 'src/app/timer.service';
 })
 export class TimerComponent implements OnInit {
 
-  constructor(public timerService: TimerService) { }
+  constructor(
+    public timerService: TimerService,
+    public dialog: MatDialog
+    ) { }
 
   ngOnInit(): void {
   }
+
+  openDialog(): void {
+    let dialogRef = this.dialog.open(CreateTimerComponent, {
+      hasBackdrop: true,
+      width: '500px'
+    });
+  }
+
 }
