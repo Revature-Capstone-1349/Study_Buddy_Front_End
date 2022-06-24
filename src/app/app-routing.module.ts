1aab04dc4ca68b67e486046836a72f6d1f73c6cc
Nicholas Torres <nicholastorres@gmx.com>
2022-06-24 10:25:48 -0400

    timer functionaility completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { TimerComponent } from './Components/timer/timer.component';
-import { CreateTimerComponent } from './Components/create-timer/create-timer.component';
 import { LoginComponent } from "./Components/login/login.component";
 import { RegisterComponent } from "./Components/register/register.component";
 import { ErrorPageComponent } from "./Pages/error-page/error-page.component";
@@ -15,8 +14,6 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomePageComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'creattimer', component: CreateTimerComponent },
-  { path: 'timer', component: TimerComponent},
   { path: '**', component: ErrorPageComponent }
 ];
 
