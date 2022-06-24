41a312316be8041840b307eff0aa1e2796d2b2ec
Jeffrey Saelee <gwfds123@gmail.com>
2022-06-24 14:55:27 -0700

    fixed folder

---
+++ src/app/Components/navbar/navbar.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { SessionsService } from 'src/app/Service/sessions.service';
+import { Router } from '@angular/router';
+import { HttpBackend } from '@angular/common/http';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  user: any;
+  logger: boolean = false;
+  call = this.checkLogger();
+
+  constructor(private sessionService: SessionsService) { }
+            
+
+
+  ngOnInit(): void {
+    this.checkLogger()
+  }
+
+  checkLogger(){
+    if(this.sessionService.checkLoggedInActive()){
+      this.user = this.sessionService.getSession("userAccount")
+    }else
+    this.logger = false
+  }
+
+  onClickLogout(){
+    this.sessionService.logout();
+  }
+  
+}
